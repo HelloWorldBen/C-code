9e914e6c3a9efd8c60fbeddbcbb1053825d459d3
HelloWorldBen <32596965+HelloWorldBen@users.noreply.github.com>
2018-02-19 18:12:05 -0500

    FileConverterService

---
+++ ConverterService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileConverter
+{
+	class ConverterService
+	{
+		private System.IO.FileSystemWatcher _watcher;
+
+		public bool Start()
+		{
+			_watcher = new System.IO.FileSystemWatcher(@"D:\temp", "*.txt");
+			_watcher.Created += FileCreated;
+			_watcher.IncludeSubdirectories = false;
+			_watcher.EnableRaisingEvents = true;
+			return true;
+
+
+
+		}
+
+
+		public bool Pause()
+		{
+			_watcher.EnableRaisingEvents = false;
+			return true;
+		}
+
+		public bool Continue()
+		{
+			_watcher.EnableRaisingEvents = true;
+
+			return true;
+		}
+		private void FileCreated(object sender, FileSystemEventArgs e)
+		{
+			string content = File.ReadAllText(e.FullPath);
+			string uppercontent = content.ToUpperInvariant();
+			var dir = Path.GetDirectoryName(e.FullPath);
+			var convertedFileName = Path.GetFileName(e.FullPath);
+			var convertedPath = Path.Combine(dir, convertedFileName);
+			File.WriteAllText(convertedPath,uppercontent);
+		}
+		public bool Stop()
+		{
+			_watcher.Dispose();
+			return true;
+
+
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace FileConverter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HostFactory.Run(serviceConfig =>
+			{
+				serviceConfig.Service<ConverterService>(serviceIntance =>
+				{
+					serviceIntance.ConstructUsing(() => new ConverterService());
+					serviceIntance.WhenStarted(execute => execute.Start());
+					serviceIntance.WhenStopped(execute => execute.Stop());
+
+					serviceIntance.WhenPaused(execute => execute.Pause());
+					serviceIntance.WhenContinued(execute => execute.Continue());
+
+					
+				});
+
+				serviceConfig.EnableServiceRecovery(recoveryOption =>
+				{
+					recoveryOption.RestartService(1);
+					recoveryOption.RestartService(1);
+					recoveryOption.RestartService(1);
+
+				});
+
+				serviceConfig.EnablePauseAndContinue();
+				serviceConfig.SetServiceName("ConvertedService");
+				serviceConfig.SetDisplayName("File Converter");
+				serviceConfig.SetDescription("Demo Service");
+
+				serviceConfig.StartAutomatically();
+			});
+
+		}
+	}
+}
